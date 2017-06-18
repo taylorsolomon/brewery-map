9abed13c40b81265069bad256388081df0a1620a
Taylor Solomon <taylor.solomon@me.com>
2017-06-17 22:58:44 -0400

    Testing stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
+      <div style={{textAlign: 'center'}}>
         <h1>Suh World</h1>
-      </div>);
+     </div>);
   }
 }
